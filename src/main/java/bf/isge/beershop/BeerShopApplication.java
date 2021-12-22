9d09384ee1f40528d00e440b6d08ac3c967fedcc
ayou <louis_r@live.fr>
2021-12-22 11:19:59 +0100

    initial commit

--- src/main/java/bf/isge/beershop/BeerShopApplication.java
+++ src/main/java/bf/isge/beershop/BeerShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeershopApplication {
+public class BeerShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeershopApplication.class, args);
+		SpringApplication.run(BeerShopApplication.class, args);
 	}
 
 }
